8b21a0011335515f16216ec6189f02c6440df8fd
shiva <shivana333@gmail.com>
2022-07-27 12:23:11 +0100

    Added splash page jsx file to components which contains a brief plan. Added this components to index.js in pages folder

---
+++ components/exclusive/SplashPage/SplashPage.jsx
@@ -0,0 +1,18 @@
+/* PLAN
+1. Title
+    - Shall we reuse the header component? Or do we need to create a new component?
+
+2. Login/Sign up button
+    - Button
+    - Add styling
+    - Link to authenticator
+
+3. Continue as guest button
+    - Button
+    - Add styling
+    - Link to the homepage!
+
+
+4. Graphics
+    - Ask CJ if we wants to add a background. 
+*/
