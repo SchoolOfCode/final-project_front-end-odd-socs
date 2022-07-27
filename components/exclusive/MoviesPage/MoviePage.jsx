405d734811fb4c11e7128a92cb5faa82663f089d
leoarthurwall <leowall93@gmail.com>
2022-07-27 13:55:53 +0100

    set up movie page folder and jsx file. Added top movies component to movies page.

---
+++ components/exclusive/MoviesPage/MoviePage.jsx
@@ -0,0 +1,49 @@
+/*
+    PLAN
+    create movie page component
+    - display 10 category components
+        - watchlist
+        - my picks
+        - top movies
+        - 90s films
+        - new releases
+        - random
+        - horror
+        - sci-fi
+        - comedy
+        - rom-coms 
+        
+
+    - add compopnents to movie page component
+    - add movie page component to pages/movies.jsx
+    - import and add the header component pages/movies.jsx - DONE
+*/
+
+import styled from "styled-components";
+import Image from "next/image";
+import { PageContainer } from "../../universal/Containers.styles";
+import TopMovies from "../HomePage/TopMovies";
+// import TopTV from "../HomePage/TopTV";
+// import TopMusic from "../HomePage/TopMusic";
+// import TopGames from "../HomePage/TopGames";
+
+const PageTitle = styled.div`
+  color: var(--secondary);
+  font-size: 3.5rem;
+`
+
+
+function MoviesPage() {
+    return (
+      
+        <PageContainer>
+        <PageTitle>Movies</PageTitle>
+            <TopMovies />
+           
+           
+        </PageContainer>
+    
+    );
+  }
+  
+  export default MoviesPage;
---
+++ pages/movies.js
@@ -0,0 +1,13 @@
+import Header from "../components/exclusive/Header/Header";
+import MoviesPage from "../components/exclusive/MoviesPage/MoviePage";
+
+function Movies() {
+  return (
+    <>
+      <Header />
+      <MoviesPage />
+    </>
+  );
+}
+
+export default Movies;
