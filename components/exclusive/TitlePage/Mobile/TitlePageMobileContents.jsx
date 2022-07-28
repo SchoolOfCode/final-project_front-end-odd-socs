7beab13ce0f9979547d20294ae470caed6d998af
Matt Davies <matt@mattdavies.dev>
2022-07-28 14:07:45 +0100

    see details function started but event listener not yet working

--- components/exclusive/TitlePage/Mobile/TitlePageMobileContents.jsx
+++ components/exclusive/TitlePage/Mobile/TitlePageMobileContents.jsx
@@ -1,5 +1,5 @@
 import styled from "styled-components";
-import { useState, useEffect } from 'react'
+import { useState, useEffect } from "react";
 
 const TitlePageContentContainer = styled.div`
   background-color: var(--primary);
@@ -15,8 +15,6 @@ const TitlePageTitleContainer = styled.div`
   position: relative;
   display: flex;
   flex-direction: column;
-  z-index: -1;
-  /* border: 2px solid green; */
   top: -7.5vh;
   height: 100%;
   padding: 0 2rem;
@@ -24,46 +22,47 @@ const TitlePageTitleContainer = styled.div`
 `;
 
 const TitlePageStatsContainer = styled.div`
-display: flex;
-color: var(--highlight-1);
-font-size: 1.2rem;
+  display: flex;
+  color: var(--highlight-1);
+  font-size: 1.2rem;
 `;
 
 const Title = styled.h1`
-    color: white;
-    font-size: 2.5rem;
-`
+  color: white;
+  font-size: 2.5rem;
+`;
 
-const Synopsis =styled.p`
-color: white;
-font-size: 1.2rem;
-
-`
-
-
+const Synopsis = styled.a`
+  color: red;
+  font-size: 1.2rem;
+  text-decoration: none;
+`;
 
 function TitlePageContents({ movieInfo }) {
+  const [synopsis, setSynopsis] = useState("");
 
-    const [synopsis, setSynopsis] = useState("")
+  useEffect(() => {
+    const shortSynopsis = `${movieInfo.plot.slice(0, 150)}...`;
+    setSynopsis(shortSynopsis);
+  }, []);
 
-    // function cutSynopsis (movieInfo) {
-        useEffect(() => {
-          const shortSynopsis = `${movieInfo.plot.slice(0, 150)}`
-        setSynopsis(shortSynopsis)   
-        },[])
-       
-// cutSynopsis(movieInfo)
+  function showFullSynopsis() {
+    // setSynopsis(movieInfo.plot);
+    console.log("clicked");
+  }
+
   return (
     <TitlePageContentContainer>
       <TitlePageTitleContainer>
         <Title>{movieInfo.title}</Title>
         <TitlePageStatsContainer>
-        <TitlePageStatsContainer>{`${movieInfo.year} | Age | Runtime | IMDb: ${movieInfo.imDbRating}`}</TitlePageStatsContainer>
+          <TitlePageStatsContainer>{`${movieInfo.year} | Age | Runtime | IMDb: ${movieInfo.imDbRating}`}</TitlePageStatsContainer>
         </TitlePageStatsContainer>
-        <Synopsis>
-        {synopsis}
-      </Synopsis>
-    </TitlePageTitleContainer>
+        {/* <Synopsis href="#" onClick={showFullSynopsis}>
+          See details
+        </Synopsis> */}
+        <a onClick={() => console.log("wow")}>Click here</a>
+      </TitlePageTitleContainer>
     </TitlePageContentContainer>
   );
 }
