7f6170d3df63c0bd3ea820b79ae9a4b2086e4585
Ryan <highrevz@hotmail.co.uk>
2022-07-28 14:03:32 +0100

    created a MyPicks page in the pages folder

---
+++ components/exclusive/MyPicksPage/MyPicksPage.jsx
@@ -0,0 +1,35 @@
+/*
+    PLAN
+    create my picks page component
+    - display 4 category components
+        - Top hits - DONE
+        - Listen list - DONE
+        - my picks - DONE
+        - 90s hits - DONE
+        - new releases - DONE
+        - random - DONE
+        - RnB - DONE
+        - Jazz - DONE
+        - Rock - DONE
+        - Country - DONE
+*/
+
+import styled from "styled-components";
+import Image from "next/image";
+import { PageContainer } from "../../universal/Containers.styles";
+
+const PageTitle = styled.div`
+  color: var(--secondary);
+  font-size: 3.5rem;
+`;
+
+function MyPicksPage() {
+  return (
+    <PageContainer>
+      <PageTitle>My Picks</PageTitle>
+      
+    </PageContainer>
+  );
+}
+
+export default MyPicksPage;
---
+++ pages/mypicks.js
@@ -0,0 +1,13 @@
+import Header from "../components/exclusive/Header/Header";
+
+
+function MyPicks() {
+  return (
+    <>
+      <Header />
+      
+    </>
+  );
+}
+
+export default MyPicks;
