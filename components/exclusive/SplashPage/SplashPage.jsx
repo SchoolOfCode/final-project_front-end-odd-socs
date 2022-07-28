24299753c9f1ff8a317327b5c3a7bdb0466ef61b
Amaan Memon <amaan-memon@Amaans-MBP.broadband>
2022-07-28 09:26:32 +0100

    no changes made from yesterday

--- components/exclusive/SplashPage/SplashPage.jsx
+++ components/exclusive/SplashPage/SplashPage.jsx
@@ -18,26 +18,51 @@
 */
 
 // import StyledFirebaseAuth from "react-firebaseui/StyledFirebaseAuth"
-import firebase from "../../../firebase/clientApp"
-import {getAuth, GoogleAuthProvider, signInWithPopup} from "firebase/auth"
-import {NavStyles, Title, TitleSpan} from "../../../components/exclusive/Header/NavMenu/NavMenu.jsx"
+// import firebase from "../../../firebase/clientApp"
+// import {getAuth, GoogleAuthProvider, signInWithPopup} from "firebase/auth"
+import {Title, TitleSpan} from "../../../components/exclusive/Header/NavMenu/NavMenu.jsx"
 import styled from "styled-components"
-import Link from "next/link"   
+import Link from "next/link" 
+import {GoogleAuthProvider, GithubAuthProvider, EmailAuthProvider, signInWithPopup, getAuth} from "firebase/auth" 
+import StyledFirebaseAuth from "react-firebaseui/StyledFirebaseAuth"
+import {app} from "../../../firebase/clientApp"
+import {auth} from "../../../firebase/clientApp"
+import firebase from "../../../firebase/clientApp"
 
-const SplashPage = ()=> {
-    
+
+const LinkStyles = styled.a`
+display: flex;
+align-items: center;
+position: relative;
+justify-content: space-between;
+font-size: 1.25rem;
+color: white;
+text-decoration: none;
+`
+const PageContainer = styled.div`
+display:flex;
+flex-direction: column;
+align-items: center;
+`
+
+const uiConfig = {
+    signInSuccessUrl: "/home",
+    signInFlow: "popup",
+    signInOptions: [auth.EmailAuthProvider]
+};
+const SplashPage = ()=> {    
 return (
     <>
-    <NavStyles>
+    <PageContainer>
     <Title >
         <TitleSpan>1</TitleSpan>
         Source
     </Title>
-    </NavStyles>
-    {/* <Link href = '/home'>
-        <a> Continue as Guest...</a>
-    </Link> */}
-    {/* <StyledFirebaseAuth uiConfig = {uiConfig} firebaseAuth={firebase.auth()}/> */}
+    <Link href = '/home'>
+        <LinkStyles> Continue as Guest...</LinkStyles>
+    </Link>
+    <StyledFirebaseAuth uiConfig = {uiConfig} firebaseAuth={firebase.auth()}/>
+    </PageContainer>
     </>
 )
 }
