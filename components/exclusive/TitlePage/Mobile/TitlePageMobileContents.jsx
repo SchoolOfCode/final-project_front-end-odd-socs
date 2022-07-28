a0cfccac8f60b5214bd02b74e3ed520bcf936006
CJ Chapman <esvagaming@gmail.com>
2022-07-28 13:29:35 +0100

    morer mobile stuff

--- components/exclusive/TitlePage/Mobile/TitlePageMobileContents.jsx
+++ components/exclusive/TitlePage/Mobile/TitlePageMobileContents.jsx
@@ -1,4 +1,5 @@
 import styled from "styled-components";
+import { useState, useEffect } from 'react'
 
 const TitlePageContentContainer = styled.div`
   background-color: var(--primary);
@@ -12,24 +13,57 @@ const TitlePageContentContainer = styled.div`
 
 const TitlePageTitleContainer = styled.div`
   position: relative;
+  display: flex;
+  flex-direction: column;
   z-index: -1;
   /* border: 2px solid green; */
-  top: -5vh;
-  height: 75px;
+  top: -7.5vh;
+  height: 100%;
   padding: 0 2rem;
+  gap: 1rem;
+`;
+
+const TitlePageStatsContainer = styled.div`
+display: flex;
+color: var(--highlight-1);
+font-size: 1.2rem;
 `;
 
 const Title = styled.h1`
     color: white;
-    font-size: 3rem;
+    font-size: 2.5rem;
 `
 
+const Synopsis =styled.p`
+color: white;
+font-size: 1.2rem;
+
+`
+
+
+
 function TitlePageContents({ movieInfo }) {
+
+    const [synopsis, setSynopsis] = useState("")
+
+    // function cutSynopsis (movieInfo) {
+        useEffect(() => {
+          const shortSynopsis = movieInfo.plot.slice(0, 150)
+        setSynopsis(shortSynopsis)   
+        },[])
+       
+// cutSynopsis(movieInfo)
   return (
     <TitlePageContentContainer>
       <TitlePageTitleContainer>
-        <Title>Title</Title>
-      </TitlePageTitleContainer>
+        <Title>{movieInfo.title}</Title>
+        <TitlePageStatsContainer>
+        <TitlePageStatsContainer>{`${movieInfo.year} | Age | Runtime | IMDb: ${movieInfo.imDbRating}`}</TitlePageStatsContainer>
+        </TitlePageStatsContainer>
+        <Synopsis>
+        {synopsis}
+      </Synopsis>
+    </TitlePageTitleContainer>
     </TitlePageContentContainer>
   );
 }
