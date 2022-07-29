88b09d69da0499bb2cc2927ee967f3873995d7ce
leoarthurwall <leowall93@gmail.com>
2022-07-29 15:05:57 +0100

    changed  webpack test config settings

--- next.config.js
+++ next.config.js
@@ -7,7 +7,7 @@ const nextConfig = {
 
   webpack: (config, options) => {
     config.module.rules.push(      {
-      test: /\.(jpe?g|png|svg|gif|ico|eot|ttf|woff|woff2|mp4|pdf|webm)$/,
+      test: /\.(ttf|woff|woff2|mp4|pdf|webm)$/,
       type: 'asset',
       generator: {
         filename: 'static/chunks/[path][name].[hash][ext]'
---
+++ pages/profile.js
@@ -0,0 +1,13 @@
+import Header from "../components/exclusive/Header/Header";
+import { ProfilePage } from "../components/exclusive/ProfilePage/ProfilePage";
+
+function Profile() {
+  return (
+    <>
+      <Header />
+      <ProfilePage />
+    </>
+  );
+}
+
+export default Profile;
