3c44d66cfc9483207665823205ddbc8234bc86e2
Amaan Memon <amaan-memon@Amaans-MBP.broadband>
2022-08-02 10:28:04 +0100

    firebase config altered to address import bugs for splash page

--- firebase/index.js
+++ firebase/index.js
@@ -1,4 +1,5 @@
 import {initializeApp} from 'firebase/app';
+import 'firebase/auth';
 
 
 const firebaseConfig = {
@@ -13,6 +14,7 @@ const firebaseConfig = {
 
  const app = initializeApp(firebaseConfig);
 
+export const auth = app.auth();
 
 
 
