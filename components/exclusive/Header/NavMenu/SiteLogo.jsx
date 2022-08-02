0bb10cccea2f5a4fb00f2ffd03426ddac86794cd
Ryan <highrevz@hotmail.co.uk>
2022-08-02 12:24:46 +0100

    made logo clickable and directs to home page

--- components/exclusive/Header/NavMenu/SiteLogo.jsx
+++ components/exclusive/Header/NavMenu/SiteLogo.jsx
@@ -1,6 +1,7 @@
 import Logo from "../../../../assets/site-logo/1Source.svg";
 import Image from "next/image";
 import styled from "styled-components";
+import Link from "next/link";
 
 const LogoContainer = styled.div`
   z-index: 1000;
@@ -10,7 +11,11 @@ const LogoContainer = styled.div`
 function SiteLogo() {
   return (
     <LogoContainer>
-      <Image src={Logo} width={200} height={100} alt="1Source Logo"></Image>
+      <Link href="/home">
+        <a>
+          <Image src={Logo} width={200} height={100} alt="1Source Logo"></Image>
+        </a>
+      </Link>
     </LogoContainer>
   );
 }
