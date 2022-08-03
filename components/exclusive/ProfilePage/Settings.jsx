6297db3928c215dc285b41b8662f53224e635459
leoarthurwall <leowall93@gmail.com>
2022-08-03 12:34:13 +0100

    profile page layout and functionality complete - for desktop. settings icon file deleted as it was hardcoded for the layout. made the settings headers functional with arrow dropdowns

--- components/exclusive/ProfilePage/Settings.jsx
+++ components/exclusive/ProfilePage/Settings.jsx
@@ -1,5 +1,4 @@
 import styled from "styled-components";
-import SettingsIcon from "./SettingsIcon";
 import { useState } from "react";
 
 import ArrowDropDownIcon from "@mui/icons-material/ArrowDropDown";
@@ -7,114 +6,171 @@ import ArrowDropUpIcon from "@mui/icons-material/ArrowDropUp";
 
 //STYLED COMPONENTS
 const ProfileSettingsContainer = styled.div`
-    display:flex;
-    flex-direction:column;
-    color:white;
-    width:30vw;
-    position:absolute;
-    top:25px;
-    right:15px;
-    gap:1.5rem;
-`
+  display: flex;
+  flex-direction: column;
+  color: white;
+  width: 30vw;
+  position: absolute;
+  top: 25px;
+  right: 15px;
+  gap: 1.5rem;
+`;
 
 const SettingHeaders = styled.h3`
-    font-weight:700;
-    font-size: 1.1rem;
-    color: var(--font-primary-color);
-`
-const HeadingRow = styled.div`
-    display: flex;
-    align-items: center;
-    justify-content: flex-end;
-    gap: 0.5rem;
-
-`
+  font-weight: 700;
+  font-size: 1.1rem;
+  color: var(--font-primary-color);
+`;
 const SettingsArrow = styled.div`
-    color: var(--font-primary-color);
-    display: flex;
-    align-items: center;
-`
+  color: var(--font-primary-color);
+  display: flex;
+  align-items: center;
+`;
+const SettingsSectionContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
 
-
-const SettingsContainer = styled.div`
-    display: flex;
-    align-items: center;
-    justify-content: flex-end;
-    gap: 0.5rem;
-`
+const SettingsTitleContainer = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: flex-end;
+  gap: 0.5rem;
+`;
 
 const DropDownContentContainer = styled.div`
-display: flex;
-gap: 0.5rem;
-flex-direction:column;
-margin-right:2.7rem;
-`
+  display: flex;
+  gap: 0.5rem;
+  flex-direction: column;
+  margin-right: 2.7rem;
+`;
 
 const DropDownContent = styled.h4`
-font-weight:500;
-color:var(--font-secondary-color);
-justify-content: flex-end;
-display: flex;
-`
+  font-weight: 500;
+  color: var(--font-secondary-color);
+  justify-content: flex-end;
+  display: flex;
+`;
 
-export function Settings(){
-    // STATES
-   const [isMyAccountOpen, setIsMyAccountOpen] = useState(false);
+export function Settings() {
+  // STATES
+  const [isMyAccountOpen, setIsMyAccountOpen] = useState(false);
+  const [isNotificationsOpen, setIsNotificationsOpen] = useState(false);
+  const [isAccessibilityOpen, setIsAccessibilityOpen] = useState(false);
+  const [isCustomerSupportOpen, setIsCustomerSupportOpen] = useState(false);
 
   //TOGGLE FUNCTIONS
-    //My Account
-    function toggleMyAccountOpen(e){
-        //What does this prevent from happening??? MATT??????
-        e.preventDefault;
-        //"Ok Google" - Matt - please explain
-        setIsMyAccountOpen((prevIsMyAccountOpen)=> !prevIsMyAccountOpen);
-    }
+  //My Account
+  function toggleMyAccountOpen(e) {
+    //What does this prevent from happening??? MATT??????
+    e.preventDefault;
+    //"Ok Google" - Matt - please explain
+    setIsMyAccountOpen((prevIsMyAccountOpen) => !prevIsMyAccountOpen);
+  }
+  function toggleNotificationsOpen(e) {
+    e.preventDefault;
+    setIsNotificationsOpen(
+      (prevIsNotificationsOpen) => !prevIsNotificationsOpen
+    );
+  }
+  function toggleAccessibilityOpen(e) {
+    e.preventDefault;
+    setIsAccessibilityOpen(
+      (prevIsAccessibilityOpen) => !prevIsAccessibilityOpen
+    );
+  }
+  function toggleCustomerSupportOpen(e) {
+    e.preventDefault;
+    setIsCustomerSupportOpen(
+      (prevIsCustomerSupportOpen) => !prevIsCustomerSupportOpen
+    );
+  }
 
-    return(
-        <ProfileSettingsContainer>
-            <SettingsContainer onClick={toggleMyAccountOpen}>
-                <SettingHeaders>My Account</SettingHeaders>
-                {/* Depending on the state, the arrow icon changes 
-                When the state is TRUE - Arrow is UP
-                When the state is FALSE - Arrow is down
-                */}
-                <SettingsArrow>
-                    {isMyAccountOpen ?(
-                        <ArrowDropUpIcon fontSize="large" />
-                        ) : (
-                        <ArrowDropDownIcon fontSize="large" />
-                    )}
-                </SettingsArrow>
-            </SettingsContainer>
-
-            {isMyAccountOpen &&(
-                <DropDownContentContainer>
-                    <DropDownContent>Log Out</DropDownContent>
-                    <DropDownContent>Delete Account</DropDownContent>
-                </DropDownContentContainer>
+  return (
+    <ProfileSettingsContainer>
+      {/* MY ACCOUNT */}
+      <SettingsSectionContainer>
+        <SettingsTitleContainer onClick={toggleMyAccountOpen}>
+          <SettingHeaders>My Account</SettingHeaders>
+          {/* Depending on the state, the arrow icon changes 
+                    When the state is TRUE - Arrow is UP
+                    When the state is FALSE - Arrow is down
+                    */}
+          <SettingsArrow>
+            {isMyAccountOpen ? (
+              <ArrowDropUpIcon fontSize="large" />
+            ) : (
+              <ArrowDropDownIcon fontSize="large" />
             )}
-            
+          </SettingsArrow>
+        </SettingsTitleContainer>
 
+        {isMyAccountOpen && (
+          <DropDownContentContainer>
+            <DropDownContent>Log Out</DropDownContent>
+            <DropDownContent>Delete Account</DropDownContent>
+          </DropDownContentContainer>
+        )}
+      </SettingsSectionContainer>
+      {/* NOTIFICATIONS */}
+      <SettingsSectionContainer>
+        <SettingsTitleContainer onClick={toggleNotificationsOpen}>
+          <SettingHeaders>Notifications</SettingHeaders>
+          <SettingsArrow>
+            {isNotificationsOpen ? (
+              <ArrowDropUpIcon fontSize="large" />
+            ) : (
+              <ArrowDropDownIcon fontSize="large" />
+            )}
+          </SettingsArrow>
+        </SettingsTitleContainer>
 
+        {isNotificationsOpen && (
+          <DropDownContentContainer>
+            <DropDownContent>Deactivate Push Notifcations</DropDownContent>
+          </DropDownContentContainer>
+        )}
+      </SettingsSectionContainer>
+         {/* ACCESSIBILITY */}
+         <SettingsSectionContainer>
+        <SettingsTitleContainer onClick={toggleAccessibilityOpen}>
+          <SettingHeaders>Accessibility</SettingHeaders>
+          <SettingsArrow>
+            {isAccessibilityOpen ? (
+              <ArrowDropUpIcon fontSize="large" />
+            ) : (
+              <ArrowDropDownIcon fontSize="large" />
+            )}
+          </SettingsArrow>
+        </SettingsTitleContainer>
 
+        {isAccessibilityOpen && (
+          <DropDownContentContainer>
+            <DropDownContent>Text Size</DropDownContent>
+          </DropDownContentContainer>
+        )}
+      </SettingsSectionContainer>
+       {/* CUSTOMER SUPPORT */}
+       <SettingsSectionContainer>
+        <SettingsTitleContainer onClick={toggleCustomerSupportOpen}>
+          <SettingHeaders>Customer Support</SettingHeaders>
+          <SettingsArrow>
+            {isCustomerSupportOpen ? (
+              <ArrowDropUpIcon fontSize="large" />
+            ) : (
+              <ArrowDropDownIcon fontSize="large" />
+            )}
+          </SettingsArrow>
+        </SettingsTitleContainer>
 
-
-
-
-            <HeadingRow>
-                <SettingHeaders>Notifications</SettingHeaders>
-                <SettingsIcon />
-            </HeadingRow>
-            <HeadingRow>
-                <SettingHeaders>Accessibility</SettingHeaders>
-                <SettingsIcon />
-            </HeadingRow>
-            <HeadingRow>
-                <SettingHeaders>Customer Support</SettingHeaders>
-                <SettingsIcon />
-            </HeadingRow>
-        </ProfileSettingsContainer>
-    )
+        {isCustomerSupportOpen && (
+          <DropDownContentContainer>
+            <DropDownContent>Contact Us</DropDownContent>
+          </DropDownContentContainer>
+        )}
+      </SettingsSectionContainer>
+    </ProfileSettingsContainer>
+  );
 }
 
 /*MATT */
