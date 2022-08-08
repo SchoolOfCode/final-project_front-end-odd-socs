ddb9e632ff1f5bade97b60d5d6133797bd2f0390
Amaan Memon <amaan-memon@Amaans-MBP.broadband>
2022-08-08 11:24:20 +0100

    log in email component started

--- components/exclusive/SplashPage/firebase/LogIn/LogInEmail.jsx
+++ components/exclusive/SplashPage/firebase/LogIn/LogInEmail.jsx
@@ -1,1 +1,16 @@
 // Log in with Email & Password component
+
+const LogInEmail = ({}) => {
+    return (
+        <div className="LogInEmail">
+        <h1>Log In with Email & Password</h1>
+        <form>
+            <h4> Please enter you email address <span> (e.g. joebloggs@gmail.com)</span></h4>
+            <input type="text" placeholder="Email" />
+            <h4> Please enter you password <span> (e.g. password123)</span></h4>
+            <input type="password" placeholder="Password" />
+            <button>Log In</button>
+        </form>
+        </div>
+    );
+    }
