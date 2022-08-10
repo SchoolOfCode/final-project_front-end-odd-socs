3dc63130b96fe1425692f222d32114eca20cb067
leoarthurwall <leowall93@gmail.com>
2022-08-10 12:02:34 +0100

    added get request to my picks movies, array of data is now displaying in the console. need to display the data on JSX next

--- components/exclusive/MoviesPage/MyPicksMovies.jsx
+++ components/exclusive/MoviesPage/MyPicksMovies.jsx
@@ -1,16 +1,45 @@
 import styled from "styled-components";
 import Image from "next/image";
 import { TitleText, SectionTitle } from "../../universal/Text.styles";
+import { app, db } from "../../../firebase/config";
+import { collection, getDocs, doc } from "firebase/firestore"
+import { useEffect, useState } from "react";
 
 import {
   ImageContainer,
   RowContainer,
   TitleContainer,
 } from "../../universal/Containers.styles";
+import { ConstructionOutlined } from "@mui/icons-material";
 
-
+/* PLAN
+  - create a state to store our data that we've retrieved from firebase (DONE)
+  - connect to the right database (DONE)
+  - create our fucntion for our GET request
+  - display the data in the jsx 
+  - run our function when the user first goes onto the mypicks page (useEffect)
+*/ 
 
 function MyPicksMovies() {
+
+  const [fireData, setFireData] = useState([]);
+  const myPicksDb = collection(db, "myPicks");
+
+  const getPicksData = async () => { 
+    await getDocs(myPicksDb)
+    .then((response) => {
+      setFireData(response.docs.map((picksData)=>{
+        return {...picksData.data(), id:picksData.id}
+    }))
+    })
+  }
+
+  useEffect(() => {
+    getPicksData()
+    .then(() => {
+      console.log(fireData)})
+  },[])
+
   return (
     <>
       <SectionTitle>My Picks - Movies</SectionTitle>
