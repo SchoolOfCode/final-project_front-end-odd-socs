87760f2e355a70ce3188a86a55019be65460b8d3
CJ Chapman <esvagaming@gmail.com>
2022-08-11 14:33:42 +0100

    added click to see bigger screenshots on games THANKS MATT

--- components/exclusive/TitlePage/Games/Desktop/GamesTitlePageDesktopMid.jsx
+++ components/exclusive/TitlePage/Games/Desktop/GamesTitlePageDesktopMid.jsx
@@ -1,5 +1,8 @@
 import styled from "styled-components";
 import Image from "next/image";
+import { useState } from "react";
+// import SearchCloseIcon from "../../../Header/Search/SearchCloseIcon";
+import CloseIcon from "@mui/icons-material/Close";
 
 import {
   TitlePageMidContainer,
@@ -33,21 +36,39 @@ const GamesMidContainerTopSection = styled(MidContainerTopSection)`
   gap: 0.5rem;
 `;
 
-// const showImgModal = e => {
-//   setModalImgUrl(screenshot.image)
-// };
-
-// let ScreenshotArray=[].map(screenshot => {
-//   return <Image key={image} 
-//     src={screenshot.image} 
-//     width={100} 
-//     height={100} 
-//     onClick={showImgModal} />});
-
+const ScreenshotModal = styled.div`
+position: absolute;
+top: 50%;
+left: 50%;
+width: 60rem;
+height: 40rem;
+transform: translate(-50%, -50%);
+background-color: darkgray;
+padding: 5rem;
+z-index: 100;
+`
 
 function GamesTitlePageDesktopMid({ gameData, screenshotData }) {
+  const [isGameModalOpen, setGameModalOpen] = useState(false);
+  const [screenshotID, setScreenshotID] = useState(null);
+  function openGamesModal(e) {
+    e.preventDefault();
+    setGameModalOpen(true);
+    const id = e.currentTarget.id;
+    setScreenshotID(id);
+  }
+
+  function closeGamesModal() {setGameModalOpen(false)}
+
   return (
     <GamesTitlePageMidContainer>
+
+      {isGameModalOpen && (
+      <ScreenshotModal>
+        <CloseIcon style={{zIndex:"1000",position:"absolute", top:"10px", right:"10px",fontSize:"3rem", color:"white"}} onClick={closeGamesModal} />
+        <Image layout="fill" src={screenshotData.results[screenshotID].image } />
+      </ScreenshotModal>
+      )}
       <GamesMidContainerTopSection>
         <ContentTitle>{gameData.name}</ContentTitle>
         <StatsContainer>{`${gameData.released.slice(0, 4)} | ${
@@ -58,7 +79,7 @@ function GamesTitlePageDesktopMid({ gameData, screenshotData }) {
         <StatsContainer>
           Play on:
           {gameData.parent_platforms.map((platform) => {
-            console.log(platform);
+            // console.log(platform);
             return ` ${platform.platform.name} |`;
           })}
         </StatsContainer>
@@ -66,9 +87,9 @@ function GamesTitlePageDesktopMid({ gameData, screenshotData }) {
       <MidContainerMidSection>
         <RowContainer>
           <ScreenshotsAllContainer>
-            {screenshotData.results.map((screenshot) => {
+            {screenshotData.results.map((screenshot,index) => {
               return (
-                <ScreenshotContainer>
+                <ScreenshotContainer id={index} onClick={openGamesModal}>
                   <Image src={screenshot.image} layout="fill"></Image>
                 </ScreenshotContainer>
               );
